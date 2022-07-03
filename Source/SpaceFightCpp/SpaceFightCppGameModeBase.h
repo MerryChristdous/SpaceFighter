8ad4b48b62fa31389623a7581bf53bbf79a330e4
MerryChristdous <77201985@qq.com>
2022-07-03 16:06:57 +0800

    init

---
+++ Source/SpaceFightCpp/SpaceFightCppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpaceFightCppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEFIGHTCPP_API ASpaceFightCppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
